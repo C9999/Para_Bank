9245bd33655033598b4fc8266ddd28acc6db58a6
Carlos Araújo <car_soad@hotmail.com>
2026-07-02 19:02:40 -0300

    ajuste 7 na pipeline

--- features/environment.py
+++ features/environment.py
@@ -1,16 +1,29 @@
+import os
+
+from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright
+
+load_dotenv()
 
 
 def before_all(context):
-    context.playwright = sync_playwright().start()
+    playwright = sync_playwright().start()
 
-    context.browser = context.playwright.chromium.launch(
-        headless=True,
-        slow_mo=700
+    headless = os.getenv("HEADLESS", "False").lower() == "true"
+    slow_mo = int(os.getenv("SLOW_MO", "0"))
+
+    browser = playwright.chromium.launch(
+        headless=headless,
+        slow_mo=slow_mo
     )
+
+    context.playwright = playwright
+    context.browser = browser
+
 
 def before_scenario(context, scenario):
     context.page = context.browser.new_page()
+
 
 def after_scenario(context, scenario):
     context.page.close()
